e338618f16b372b9604905b950a90d45c10ac9db
gefersonholdorf <gefersonholdorf88@gmail.com>
2024-12-02 11:52:15 -0300

    Configuração do TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -10,9 +11,20 @@ import { ConfigModule } from '@nestjs/config';
         `.env.${process.env.NODE_ENV || 'development'}`,
       ],
       isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.BD_HOST,
+      port: parseInt(process.env.BD_PORT),
+      username: process.env.BD_USER,
+      password: process.env.BD_PASSWORD,
+      database: process.env.BD_DATABASE,
+      entities: [],
+      synchronize: process.env.NODE_ENV == 'development'
     })
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
